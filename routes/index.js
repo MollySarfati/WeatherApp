3dfb9d8db61fe9fce640a636ffd6aab706a704cb
Molly <mollysarfati5@gmail.com>
2018-06-19 17:06:09 +0200

    maj app.js

--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 
+
+
 router.get('/', function(req, res, next) {
 
   req.session.cityList = [{
       ville: "Paris",
-      picto: "/images/picto-1.png",
+      picto: "./images/picto-1.png",
       etat: "Couvert",
       Tmatin: "9°C",
       Tapmidi: "10°C"
     },
     {
       ville: "Marseille",
-      picto: "/images/picto-1.png",
+      picto: "./images/picto-1.png",
       etat: "Couvert",
       Tmatin: "10°C",
       Tapmidi: "1°C"
     },
     {
       ville: "Lyon",
-      picto: "/images/picto-1.png",
+      picto: "./images/picto-1.png",
       etat: "bruine légère",
       Tmatin: "6°C",
       Tapmidi: "3°C"
     },
     {
       ville: "Lille",
-      picto: "/images/picto-1.png",
+      picto: "./images/picto-1.png",
       etat: "Couvert",
       Tmatin: "10°C",
       Tapmidi: "10°C"
@@ -42,7 +44,7 @@ router.post('/add-city', function(req, res, next) {
 
   var newRow = [{
     ville: "",
-    picto: "/images/picto-1.png",
+    picto: "./images/picto-1.png",
     etat: "Pluie",
     Tmatin: "-20°C",
     Tapmidi: "60°C"

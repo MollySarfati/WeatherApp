0e9f01f6fc1fec9d5dfeb6eef600022fca601b30
Julien CHAMPOUDRY <julien.champoudry@gmail.com>
2018-06-19 11:14:08 +0200

    route cityList added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+var cityList=[
+          {ville: "Paris", picto:"/images/picto-1.png", etat:"Couvert",Tmatin:"9°C", Tapmidi:"10°C"},
+          {ville: "Marseille", picto:"/images/picto-1.png", etat:"Couvert",Tmatin:"10°C", Tapmidi:"1°C"},
+          {ville: "Lyon", picto:"/images/picto-1.png", etat:"bruine légère",Tmatin:"6°C", Tapmidi:"3°C"},
+          {ville: "Lille", picto:"/images/picto-1.png", etat:"Couvert",Tmatin:"10°C", Tapmidi:"10°C"}
+];
+
+  res.render('index',{cityList:cityList});
 });
 
 module.exports = router;

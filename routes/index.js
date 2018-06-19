8f8f4c3a4c0490e1cd4eefa0ac0cc2915c9d91ea
Julien CHAMPOUDRY <julien.champoudry@gmail.com>
2018-06-19 14:26:59 +0200

    demarrer barre text ok

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,65 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
 
-var cityList=[
-          {ville: "Paris", picto:"/images/picto-1.png", etat:"Couvert",Tmatin:"9°C", Tapmidi:"10°C"},
-          {ville: "Marseille", picto:"/images/picto-1.png", etat:"Couvert",Tmatin:"10°C", Tapmidi:"1°C"},
-          {ville: "Lyon", picto:"/images/picto-1.png", etat:"bruine légère",Tmatin:"6°C", Tapmidi:"3°C"},
-          {ville: "Lille", picto:"/images/picto-1.png", etat:"Couvert",Tmatin:"10°C", Tapmidi:"10°C"}
+var cityList = [{
+    ville: "Paris",
+    picto: "/images/picto-1.png",
+    etat: "Couvert",
+    Tmatin: "9°C",
+    Tapmidi: "10°C"
+  },
+  {
+    ville: "Marseille",
+    picto: "/images/picto-1.png",
+    etat: "Couvert",
+    Tmatin: "10°C",
+    Tapmidi: "1°C"
+  },
+  {
+    ville: "Lyon",
+    picto: "/images/picto-1.png",
+    etat: "bruine légère",
+    Tmatin: "6°C",
+    Tapmidi: "3°C"
+  },
+  {
+    ville: "Lille",
+    picto: "/images/picto-1.png",
+    etat: "Couvert",
+    Tmatin: "10°C",
+    Tapmidi: "10°C"
+  }
 ];
 
-  res.render('index',{cityList:cityList});
+
+router.get('/', function(req, res, next) {
+
+  res.render('index', {
+    cityList: cityList
+  });
 });
 
-router.post('add-city', function(req, res, next) {
 
+router.post('/add-city', function(req, res, next) {
 
-  res.render('index',{});
+  var newRow = [{
+    ville: "",
+    picto: "/images/picto-1.png",
+    etat: "Pluie",
+    Tmatin: "-20°C",
+    Tapmidi: "60°C"
+  }];
+
+  newRow[0].ville= req.body.ville;
+
+  console.log(req.body.ville);
+
+  cityList = (cityList.concat(newRow));
+
+  res.render('index', {
+    cityList
+  });
 });
 
 

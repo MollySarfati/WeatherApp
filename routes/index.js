c64309f49df64403925de6684e42d373b493e92e
Molly <mollysarfati5@gmail.com>
2018-06-20 17:21:57 +0200

    api open weather map conf.

--- routes/index.js
+++ routes/index.js
@@ -19,10 +19,10 @@ const helper = new OpenWeatherMapHelper(
 router.get('/', function(req, res, next) {
 
     req.session.cityList = [
-      {ville: "Paris", picto: "./images/picto-1.png",etat: "Couvert",Tmatin: "9°C",Tapmidi: "10°C"},
-      {ville: "Marseille",picto: "./images/picto-1.png",etat: "Couvert",Tmatin: "10°C",Tapmidi: "1°C"},
-      {ville: "Lyon", picto: "./images/picto-1.png", etat: "bruine légère", Tmatin: "6°C", Tapmidi: "3°C"},
-      {ville: "Lille", picto: "./images/picto-1.png", etat: "Couvert", Tmatin: "10°C", Tapmidi: "10°C"}
+      {name: "Paris", picto: "./images/picto-1.png",etat: "Couvert",Tmatin: "9°C",Tapmidi: "10°C"},
+      {name: "Marseille",picto: "./images/picto-1.png",etat: "Couvert",Tmatin: "10°C",Tapmidi: "1°C"},
+      {name: "Lyon", picto: "./images/picto-1.png", etat: "bruine légère", Tmatin: "6°C", Tapmidi: "3°C"},
+      {name: "Lille", picto: "./images/picto-1.png", etat: "Couvert", Tmatin: "10°C", Tapmidi: "10°C"}
   ];
 
 /*
@@ -34,42 +34,59 @@ router.get('/', function(req, res, next) {
 });
 */
 
-var search = "http://api.openweathermap.org/data/2.5/weather?q=";
-search += req.body.ville;
-search += "&APPID=KEYS";
-request(search, function(error, response, body) {
 
-body = JSON.parse(body);
-console.log(body);
 
 res.render('index', {cityList: req.session.cityList });
 
 });
-
-});
-
+/*
+var cityList = [
+	{name: "Marseille",
+	min:14,
+	max:18,
+	picto1:"/images/picto-1.png",
+	picto2:"/images/picto-1.png",
+	commentaires: "Couvert"}
+];
+  cityList.push({name: req.body.city, min:14, max:18, picto1:"/images/picto-1.png", picto2:"/images/picto-1.png",commentaires:"soleil"});
+*/
 
 
 router.post('/add-city', function(req, res, next) {
 
-  req.session.newRow = [{
-    ville: "",
-    picto: "./images/picto-1.png",
-    etat: "Pluie",
-    Tmatin: "-20°C",
-    Tapmidi: "60°C"
-  }];
+//OpenWeatherMap
+var search = "http://api.openweathermap.org/data/2.5/weather?q=";
+search += req.body.name;
+console.log(req.body);
 
-  req.session.newRow[0].ville = req.body.ville;
+search += "&units=metric&APPID=KEYS";
+request(search, function(error, response, body) {
 
-  console.log(req.body.ville);
-  req.session.cityList = (req.session.cityList.concat(req.session.newRow));
 
-  /*  OU BIEN
-  cityList.push({name: req.body.name, picto1:"/images/picto-1.png", picto2:"/images/picto-1.png",picto3:"/images/picto-1.png",picto4:"/images/picto-1.png"})
-*/
+body = JSON.parse(body);
 
-  res.render('index', {cityList:req.session.cityList});
+
+var icon = "http://openweathermap.org/img/w/";
+icon+=body.weather[0].icon;
+icon+=".png"
+console.log(icon);
+
+///////fin api debut push
+req.session.newRow = [{
+  name: "",
+  picto: icon,
+  etat: body.weather[0].main,
+  Tmatin: body.main.temp_min,
+  Tapmidi: body.main.temp_max
+}];
+
+req.session.newRow[0].name = req.body.name;
+
+req.session.cityList = (req.session.cityList.concat(req.session.newRow));
+
+
+  res.render('index', {cityList:req.session.cityList, });
+});
 });
 
 
